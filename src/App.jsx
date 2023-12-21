72521ea7af2a6460b4bd6b245870b55b58c84198
Hannah Lindbäck <hannahlindback@gmail.com>
2023-12-21 15:00:43 +0100

    Tried adding all unicorns but broken
    : (
    
    Co-Authored-By: Sebastian Ledung <64317746+seledun@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { useState, useEffect } from "react";
 import StartScreen from "./components/StartScreen/index.jsx";
 import "./App.css";
+import {Modal, Button } from 'react-bootstrap' //For pop-ups, t.ex
+//ToDO: GET alla enhörningar till framsidan (kartan) som klickbara objekt
+//Todo: GET specifik enhörning (när användaren klickat på en enhörning)
+//ToDO: Generera ny fabel, med sparfunktion (om användaren gillar den)
+const baseURL = 'http://127.0.0.1:5000/0.0.1/'
 
 
 //GET för alla fabler som en lista (titlar)
@@ -10,8 +15,69 @@ async function getFables() {
 
   return response;
 }
-//TODO:GET för specifik fabel med titel och text
-async function getWholeFable(id) {
+async function listUnicorns() {
+  const options = {
+      method: "GET",
+      headers: {
+          "Accept": "application/json"
+      }
+  };
+  const response = await fetch(baseURL + "unicorns", options);
+  const unicorns = await response.json();
+
+ // document.getElementById("unicorn-list").innerHTML = unicorns.map(unicorn => <li>${unicorn.id}</li>).join("");
+
+ return unicorns
+}
+
+
+async function fetchABunchOfUniqueRandomUnicorns(range) {
+  const options = {
+    method: "GET",
+    headers: {
+      "Accept": "application/json"
+    }
+  };
+
+  const unicornList = []
+  const idList = []
+  const unicorns = await listUnicorns()
+  
+  for (let i in unicorns) {
+      idList.push(unicorns[i].id)
+  };
+
+  for (let i = 0; i < range; i++) {
+    let randomId = Math.floor(Math.random() * idList.length);
+
+    // Genererar ett slumpmässigt index
+    let id = idList[randomId];
+    idList.splice(randomId, 1);
+    let response = await fetch(baseURL + "unicorns/" + id, options)
+    let unicorn = await response.json();
+    unicornList.push(unicorn);
+  }
+
+  console.log(unicornList)
+  document.getElementById("unicorn-list").innerHTML = unicornList.join("");
+  return unicornList;
+}
+
+  // Pick a number of random unicorns
+  // Fetch the random unicorns
+
+
+
+  // Send unicorn
+
+function App() {
+  const [start, setStart] = useState(false);
+  const [listElements, setListElements] = useState([]);
+  const [individualFable, setIndividualFable] = useState([]);
+  const [storyBoardActive, setStoryBoardActive] = useState([]);
+
+//GET för individuell fabel när man klickar i listan 
+  async function getWholeFable(id) {
   const response = await fetch('http://127.0.0.1:5000/0.0.1/fables/'+id);
   const data = await response.json();
   setIndividualFable(data);
@@ -19,18 +85,19 @@ async function getWholeFable(id) {
   return data;
 }
 
-
-function App() {
-  const [start, setStart] = useState(false);
-  const [listElements, setListElements] = useState([]);
-  const [individualFable, setIndividualFable] = useState([]);
-
   useEffect(() => {
     getFables().then((data) => {
       console.log(data);
       setListElements(data);
+
     });
-  }, [])  
+  }, [storyBoardActive])
+  
+  useEffect(() => {
+
+    console.log(fetchABunchOfUniqueRandomUnicorns(5))
+
+  }, [])
 
   //Vad ska vi begära?
   //Endpoints
@@ -61,12 +128,10 @@ function App() {
       </ul>
       <p>One fable</p>
       <ul>
-      { individualFable.map((fable) => (
-          <section key={fable.id}>
-            <strong>{fable.name}</strong><br />
-            <strong>{fable.text}</strong><br />
+          <section key={individualFable.id}>
+            <strong>{individualFable.name}</strong><br />
+            <strong>{individualFable.text}</strong><br />
           </section>
-        )) }
       </ul>
 
     
