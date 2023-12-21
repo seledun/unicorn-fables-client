8846ffed4ce64611aa9bda573057ec8b1d147561
Hannah Lindbäck <hannahlindback@gmail.com>
2023-12-21 13:00:23 +0100

    Function for single fables
    - Added buttons for clicking on listelements to read whole single fable

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,28 @@ import { useState, useEffect } from "react";
 import StartScreen from "./components/StartScreen/index.jsx";
 import "./App.css";
 
-async function getFables() {
-    const response = await fetch('http://127.0.0.1:5000/0.0.1/fables')
-    .then(response => response.json());
 
-    return response;
+//GET för alla fabler som en lista (titlar)
+async function getFables() {
+  const response = await fetch('http://127.0.0.1:5000/0.0.1/fables')
+  .then(response => response.json());
+
+  return response;
 }
+//TODO:GET för specifik fabel med titel och text
+async function getWholeFable(id) {
+  const response = await fetch('http://127.0.0.1:5000/0.0.1/fables/'+id);
+  const data = await response.json();
+  setIndividualFable(data);
+
+  return data;
+}
+
 
 function App() {
   const [start, setStart] = useState(false);
   const [listElements, setListElements] = useState([]);
+  const [individualFable, setIndividualFable] = useState([]);
 
   useEffect(() => {
     getFables().then((data) => {
@@ -34,20 +46,30 @@ function App() {
     return <StartScreen handleClick={handleClick} />;
   }
 
-  
 
   return (
     <div className="wrapper">
       <h1>Some Title</h1>
-      <p>This is some text because I am too lazy to fetch lorem ipsum</p>
+      <p>List of fable names</p>
       <ul>
       { listElements.map((fable) => (
           <li key={fable.id}>
-            <strong>{fable.name}</strong><br />
+            <strong>{fable.name}</strong><button onClick={() => getWholeFable(fable.id)}></button><br/>
             <strong>{fable.votes}</strong><br />
           </li>
         )) }
-      </ul>    
+      </ul>
+      <p>One fable</p>
+      <ul>
+      { individualFable.map((fable) => (
+          <section key={fable.id}>
+            <strong>{fable.name}</strong><br />
+            <strong>{fable.text}</strong><br />
+          </section>
+        )) }
+      </ul>
+
+    
     </div>
   );
 }
