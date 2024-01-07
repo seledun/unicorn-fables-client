65b0b4c4ba7c83bae727ec30ee86fdf8b49b8fbd
Hannah Lindbäck <hannahlindback@gmail.com>
2024-01-07 17:07:24 +0100

    added loading spinner at generation
    wo! and the user cannot escape the generation!

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -2,6 +2,8 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { generateFable } from "../../utility/api.js";
 import "./style.css";
+import { useState } from "react";
+import { Spinner } from "react-bootstrap";
 
 function Popup({
   handleClose,
@@ -14,12 +16,14 @@ function Popup({
   const { name, description, image, id } = selectedUnicorn;
   const { bgImage } = storyParameters;
   const baseURL = "http://127.0.0.1:5000/0.0.1/";
+  const [loading, setLoading] = useState(false);
 
   console.log(setSelectedFable);
   console.log(setCurrentPage);
 
   //Generar en ny fabel
   async function generateFable() {
+    setLoading(true);
     let mood = "";
     if (bgImage == 1) {
       mood = "happy";
@@ -51,34 +55,47 @@ function Popup({
   return (
     <>
       <div className="pop-up">
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
+        <Modal
+          show={show}
+          onHide={handleClose}
+          backdrop={loading ? "static" : true}
+          keyboard={loading ? false : true}
+        >
+          <Modal.Header closeButton={!loading ? true : false}>
             <Modal.Title>{name}</Modal.Title>
           </Modal.Header>
-          <Modal.Body className="popup-body">
-            <div className="text">{description}</div>
-            <div className="img">
-              <img
-                src={image}
-                alt={name}
-                style={{ maxWidth: 200, height: "auto" }}
-              />
-            </div>
-          </Modal.Body>
-          <Modal.Footer>
-            <div className="footer-content">
-              {" "}
-              <div className="modal-button">
-              <Button
-                variant="generate"
-                onClick={(e) => generateFable(bgImage, id)}
-          
-              >
-                Generera fabel
-              </Button>
-              </div>
-            </div>
-          </Modal.Footer>
+
+          {!loading ? (
+            <>
+              <Modal.Body className="popup-body">
+                <div className="text">{description}</div>
+                <div className="img">
+                  <img
+                    src={image}
+                    alt={name}
+                    style={{ maxWidth: 200, height: "auto" }}
+                  />
+                </div>
+              </Modal.Body>
+              <Modal.Footer>
+                <div className="footer-content">
+                  {" "}
+                  <div className="modal-button">
+                    <Button
+                      variant="generate"
+                      onClick={(e) => generateFable(bgImage, id)}
+                    >
+                      Generera fabel
+                    </Button>
+                  </div>
+                </div>
+              </Modal.Footer>
+            </>
+          ) : (
+            <Modal.Body className="popup-body">
+              <Spinner animation="border" variant="success"></Spinner>
+            </Modal.Body>
+          )}
         </Modal>
       </div>
     </>
